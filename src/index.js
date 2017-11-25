1676c76c7694cda162801f65dce083b076cce61e
Jeremy Blalock <jeremyjblalock@gmail.com>
2017-11-24 17:59:12 -0800

    Gets compilation working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello?")
+import React, { Component } from 'react'
+
+export default class MultiMenu extends Component {
+  render() {
+    return (
+      <h1>Hello, world!</h1>
+    )
+  }
+}
