2588a9d076349c6f23e8bc69b935483ef0f97da8
Jeremy Blalock <jeremyjblalock@gmail.com>
2017-11-24 23:01:08 -0800

    Gets top-level working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,115 @@
 import React, { Component } from 'react'
 
-export default class MultiMenu extends Component {
+// Positions are symbols to prevent hardcoding values.
+// The only way to reference position is by directly importing.
+export const TOP = Symbol('TOP')
+export const TOP_LEFT = Symbol('TOP_LEFT')
+export const TOP_RIGHT = Symbol('TOP_RIGHT')
+
+export const BOTTOM = Symbol('BOTTOM')
+export const BOTTOM_LEFT = Symbol('BOTTOM_LEFT')
+export const BOTTOM_RIGHT = Symbol('BOTTOM_RIGHT')
+
+export const DEFAULT_POSITION = BOTTOM
+
+export const matches = (openPath, path) => {
+  for (let i = 0; i < path.length; i += 1) {
+    if (openPath[i] !== path[i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export default class MultiMenuWrapper extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      openPath: []
+    }
+  }
+
+  handleHover = openPath => {
+    this.setState({ openPath })
+  }
+
   render() {
+    let { align, isSubMenu, menu, position, onSelect } = this.props
+    let { openPath } = this.state
+
     return (
-      <h1>Hello, world!</h1>
+      <div>
+        <MultiMenu
+          {...{ isSubMenu, onSelect }}
+          basePath={[]}
+          menu={menu}
+          openPath={openPath}
+          onHover={this.handleHover}
+        />
+      </div>
+    )
+  }
+}
+
+export class MultiMenu extends Component {
+  render() {
+    let {
+      basePath,
+      isSubMenu,
+      menu,
+      onHover,
+      onSelect,
+      openPath,
+    } = this.props
+
+    return (
+      <div className="multi-menu">
+        {menu.map((itm, i) => (
+          <MenuItem
+            key={i}
+            data={itm}
+            onHover={onHover}
+            onSelect={onSelect}
+            openPath={openPath}
+            path={basePath.concat([i])}
+          />
+        ))}
+      </div>
+    )
+  }
+}
+
+export class MenuItem extends Component {
+  handleClick = e => {
+    let { data: { value }, onSelect } = this.props
+
+    onSelect(value)
+  }
+
+  handleHover = e => {
+    let { path, onHover } = this.props
+
+    onHover(path)
+  }
+
+  render() {
+    let { data, path, onHover, onSelect, openPath } = this.props
+
+    return (
+      <div className="multi-menu-item" onMouseMove={this.handleHover}>
+        {data.label}
+        {matches(openPath, path) && data.children && data.children.length > 0
+          ? <MultiMenu
+              basePath={path}
+              menu={data.children}
+              onHover={onHover}
+              onSelect={onSelect}
+              openPath={openPath}
+            />
+          : null}
+      </div>
     )
   }
 }
--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import MultiMenu from '../src'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+const menuData = [
+  { label: 'Item 1', value: 'item1' },
+  { label: 'Item 2', value: 'item2' },
+  { label: 'Item 3', value: 'item3' },
+  {
+    label: 'Item 4',
+    value: 'item4',
+    children: [
+      { label: 'Child 1', value: 'child1' },
+      {
+        label: 'Child 2',
+        value: 'child2',
+        children: [
+          { label: 'Sub-child 1', value: 'sc1' },
+          { label: 'Sub-child 2', value: 'sc2' },
+        ]
+      }
+    ]
+  },
+]
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('MultiMenu', module)
+  .add('basic', () => (
+    <MultiMenu
+      menu={menuData}
+    />
+  ))
